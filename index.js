bd91047ead5ff41b8bae48ed4362d038cb80e911
Donna Amos <donnacamos.developer@gmail.com>
2019-09-16 02:30:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function logDriverNames(drivers){
 }
 
 function logDriversByHometown(drivers, location){
-    selectedDrivers = drivers.filter(driver => driver.hometown === location);
+  function selectedDrivers = drivers.filter(driver => driver.hometown === location);
     logDriverNames(selectedDrivers);
 }
 
